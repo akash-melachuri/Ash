934e3956047891e14464e3cd1b3b49b35e55b13d
javalich <javalich@outlook.com>
2021-01-30 14:29:35 -0500

    Transitioned image for staging buffer destination and for reading in shaders

--- Engine/src/VulkanAPI.cpp
+++ Engine/src/VulkanAPI.cpp
@@ -1231,6 +1231,16 @@ void VulkanAPI::createTextureImage() {
                 VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL,
                 VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
                 textureImage, textureImageAllocation);
+
+    transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB,
+                          VK_IMAGE_LAYOUT_UNDEFINED,
+                          VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+    copyBufferToImage(stagingBuffer, textureImage,
+                      static_cast<uint32_t>(texWidth),
+                      static_cast<uint32_t>(texHeight));
+    transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB,
+                          VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+                          VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 }
 
 void VulkanAPI::createSurface() {
